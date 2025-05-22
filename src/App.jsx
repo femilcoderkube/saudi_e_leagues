d501146b53c6f64c046363d59cc308e775b29542
Kishan <kishansutariya.coderkube@gmail.com>
2025-05-22 12:55:12 +0530

    init vite

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Vite + React</h1>
+    </>
+  );
+}
+
+export default App;
