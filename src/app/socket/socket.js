fb27ab719ecb580ce43b24ece4aa21fda1871d7e
Kishan <kishansutariya.coderkube@gmail.com>
2025-06-17 11:15:34 +0530

    changes

--- src/app/socket/socket.js
+++ src/app/socket/socket.js
@@ -1,31 +1,38 @@
+import { io } from "socket.io-client";
+import { store } from "../slices/store";
+import { setSocketConnected, setSocketId } from "../slices/socket/socketSlice";
+import {
+  setLeagueData,
+  setJoinStatus,
+  setLeagueError,
+} from "../slices/leagueDetail/leagueDetailSlice";
+import { SOCKET } from "../../utils/constant";
 
-import { io } from 'socket.io-client';
-import { store } from '../slices/store';
-import { setSocketConnected, setSocketId } from '../slices/socket/socketSlice';
-import { setLeagueData, setJoinStatus, setLeagueError } from '../slices/leagueDetail/leagueDetailSlice';
-import { SOCKET } from '../../utils/constant';
-
-const SOCKET_URL = "/";
+// const SOCKET_URL = "/";
+const SOCKET_URL = import.meta.env.VITE_SOCKET_URL || "/";
 
 export const socket = io(SOCKET_URL, {
-  transports: ['websocket'],
-
+  transports: ["websocket"],
 });
 
-socket.on('connect', () => {
-  console.log('Socket connected:', socket.id);
+socket.on("connect", () => {
+  console.log("Socket connected:", socket.id);
   store.dispatch(setSocketConnected(true));
   store.dispatch(setSocketId(socket.id));
 });
 
-socket.on('disconnect', (reason) => {
-  console.log('Socket disconnected:', reason);
+socket.on("disconnect", (reason) => {
+  console.log("Socket disconnected:", reason);
   store.dispatch(setSocketConnected(false));
   store.dispatch(setSocketId(null));
 });
 
-socket.on('connect_error', (error) => {
-  console.error('Socket connection error:', error.message, error.stack, error.name);
+socket.on("connect_error", (error) => {
+  console.error(
+    "Socket connection error:",
+    error.message,
+    error.stack,
+    error.name
+  );
   store.dispatch(setSocketConnected(false));
 });
-
