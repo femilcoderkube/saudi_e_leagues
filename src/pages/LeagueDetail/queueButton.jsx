2094816e5eee4f0dd3026d433773d0ae3dddaa9a
ashraf-ck <134932420+ashrafcoderkube@users.noreply.github.com>
2025-08-19 19:23:48 +0530

    asdfasd

--- src/pages/LeagueDetail/queueButton.jsx
+++ src/pages/LeagueDetail/queueButton.jsx
@@ -33,6 +33,7 @@ const GetQueueButton = () => {
       <div
         className="common-width lobby_btn mb-8 relative cursor-pointer flex items-center justify-center md:justify-center"
         onClick={() => {
+          let type = localStorage.getItem("deviceType")
           if(type== "mobile"){
             window.AndroidInterface?.signInCallbackHandler(`${window.location.href}`);
             window.webkit?.messageHandlers?.jsMessageHandler?.signInCallbackHandler(`${window.location.href}`);
