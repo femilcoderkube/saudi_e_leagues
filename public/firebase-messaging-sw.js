da21d5b76187c7b6bc73849b35243f55f8fbbf9b
ashraf-ck <134932420+ashrafcoderkube@users.noreply.github.com>
2025-08-22 19:12:31 +0530

    asdasdas

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,40 +1,40 @@
-// // Scripts for firebase and firebase messaging
-// importScripts('https://www.gstatic.com/firebasejs/10.13.2/firebase-app-compat.js');
-// importScripts('https://www.gstatic.com/firebasejs/10.13.2/firebase-messaging-compat.js');
+// Scripts for firebase and firebase messaging
+importScripts('https://www.gstatic.com/firebasejs/10.12.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.12.0/firebase-messaging-compat.js');
 
-// // Initialize the Firebase app in the service worker
-// // "Default" Firebase configuration (prevents errors)
-// const firebaseConfig = {
-//   apiKey: "AIzaSyAr6qxfWZaQ6-9Xq_2qLoYHR-uFA7A6eZc",
-//   authDomain: "stagingprime.firebaseapp.com",
-//   projectId: "stagingprime",
-//   storageBucket: "stagingprime.firebasestorage.app",
-//   messagingSenderId: "408870663796",
-//   appId: "1:408870663796:web:bb28b8cded171d1f4bc321",
-//   measurementId: "G-R273JZNFD2"
-// };
+// Initialize the Firebase app in the service worker
+// "Default" Firebase configuration (prevents errors)
+const firebaseConfig = {
+  apiKey: "AIzaSyAr6qxfWZaQ6-9Xq_2qLoYHR-uFA7A6eZc",
+  authDomain: "stagingprime.firebaseapp.com",
+  projectId: "stagingprime",
+  storageBucket: "stagingprime.firebasestorage.app",
+  messagingSenderId: "408870663796",
+  appId: "1:408870663796:web:bb28b8cded171d1f4bc321",
+  measurementId: "G-R273JZNFD2"
+};
 
-// firebase.initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig);
 
-// // Retrieve firebase messaging
-// const messaging = firebase.messaging();
-// messaging.onBackgroundMessage((payload) => {
-//   console.log("asasasas--onBackgroundMessage",payload)
-//   const notificationTitle = payload.notification.title;
-//   const notificationOptions = {
-//     body: payload.notification.body,
-//     // icon: payload.notification.image,
-//   };
+// Retrieve firebase messaging
+const messaging = firebase.messaging();
+messaging.onBackgroundMessage((payload) => {
+  console.log("asasasas--onBackgroundMessage",payload)
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+    // icon: payload.notification.image,
+  };
 
-//   self.registration.showNotification(notificationTitle, notificationOptions);
-// });
-// messaging.onMessage((payload) => {
-//   console.log("asasasas--onMessage",payload)
-//   const notificationTitle = payload.notification.title;
-//   const notificationOptions = {
-//     body: payload.notification.body,
-//     // icon: payload.notification.image,
-//   };
+  self.registration.showNotification(notificationTitle, notificationOptions);
+});
+messaging.onMessage((payload) => {
+  console.log("asasasas--onMessage",payload)
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+    // icon: payload.notification.image,
+  };
 
-//   self.registration.showNotification(notificationTitle, notificationOptions);
-// });
+  self.registration.showNotification(notificationTitle, notificationOptions);
+});
