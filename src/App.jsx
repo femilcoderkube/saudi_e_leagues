b9ae17616b955a685d2dd45682594fbc4fab80e8
ashraf-ck <134932420+ashrafcoderkube@users.noreply.github.com>
2025-08-22 19:09:26 +0530

    asdfasd

--- src/App.jsx
+++ src/App.jsx
@@ -44,7 +44,22 @@ function NavigatorSetter() {
   return null;
 }
 
+import { getMessaging } from "firebase/messaging/sw";
+import { onBackgroundMessage } from "firebase/messaging/sw";
 
+const messaging1 = getMessaging();
+onBackgroundMessage(messaging1, (payload) => {
+  console.log('[firebase-messaging-sw.js] Received background message ', payload);
+  // Customize notification here
+  const notificationTitle = 'Background Message Title';
+  const notificationOptions = {
+    body: 'Background Message body.',
+    icon: '/favicon.png'
+  };
+
+  self.registration.showNotification(notificationTitle,
+    notificationOptions);
+});
 
 
 async function requestPermission() {
