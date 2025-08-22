7bd4e789d13db0e082c757e342669c4b0eda7f12
ashraf-ck <134932420+ashrafcoderkube@users.noreply.github.com>
2025-08-22 19:07:42 +0530

    adsasd

--- src/firebase.js
+++ src/firebase.js
@@ -19,3 +19,19 @@ const app = initializeApp(firebaseConfig);
 // Messaging service
 export const messaging = getMessaging(app);
 
+import { getMessaging } from "firebase/messaging/sw";
+import { onBackgroundMessage } from "firebase/messaging/sw";
+
+const messaging1 = getMessaging();
+onBackgroundMessage(messaging1, (payload) => {
+  console.log('[firebase-messaging-sw.js] Received background message ', payload);
+  // Customize notification here
+  const notificationTitle = 'Background Message Title';
+  const notificationOptions = {
+    body: 'Background Message body.',
+    icon: '/favicon.png'
+  };
+
+  self.registration.showNotification(notificationTitle,
+    notificationOptions);
+});
