7f1d3347e93202caf7de8f47e9d2458d297f4210
ashraf-ck <134932420+ashrafcoderkube@users.noreply.github.com>
2025-08-22 19:22:16 +0530

    asdas

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -18,23 +18,23 @@ firebase.initializeApp(firebaseConfig);
 
 // Retrieve firebase messaging
 const messaging = firebase.messaging();
-messaging.onBackgroundMessage((payload) => {
-  console.log("asasasas--onBackgroundMessage",payload)
-  const notificationTitle = payload.notification.title;
-  const notificationOptions = {
-    body: payload.notification.body,
-    // icon: payload.notification.image,
-  };
+// messaging.onBackgroundMessage((payload) => {
+//   console.log("asasasas--onBackgroundMessage",payload)
+//   const notificationTitle = payload.notification.title;
+//   const notificationOptions = {
+//     body: payload.notification.body,
+//     // icon: payload.notification.image,
+//   };
 
-  self.registration.showNotification(notificationTitle, notificationOptions);
-});
-messaging.onMessage((payload) => {
-  console.log("asasasas--onMessage",payload)
-  const notificationTitle = payload.notification.title;
-  const notificationOptions = {
-    body: payload.notification.body,
-    // icon: payload.notification.image,
-  };
+//   self.registration.showNotification(notificationTitle, notificationOptions);
+// });
+// messaging.onMessage((payload) => {
+//   console.log("asasasas--onMessage",payload)
+//   const notificationTitle = payload.notification.title;
+//   const notificationOptions = {
+//     body: payload.notification.body,
+//     // icon: payload.notification.image,
+//   };
 
-  self.registration.showNotification(notificationTitle, notificationOptions);
-});
+//   self.registration.showNotification(notificationTitle, notificationOptions);
+// });
