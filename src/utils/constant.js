39af0547f43b2ba72025301e3313ed2e3e9fba4d
meet9171 <meetkathiriya.coderkube@gmail.com>
2025-09-08 11:19:14 +0530

    IMPORT ISSUE

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -3,7 +3,7 @@ import prime_hover from "../assets/images/prime_hover.png";
 import LargePrime from "../assets/images/prime_hover.png";
 import axiosInstance, { baseURL } from "./axios";
 import moment from "moment-timezone";
-import { Prime } from "../components/UIComponents/index.jsx";
+import { Prime } from "../components/ui/svg/index.jsx";
 
 export const items = [
   {
