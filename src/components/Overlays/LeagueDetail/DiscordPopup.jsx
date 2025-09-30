d66cfc3f0bf750dd4a7577ed92a103ba5e05ff75
Kishan <femil.coderkube@gmail.com>
2025-09-30 16:59:10 +0530

    changes

--- src/components/Overlays/LeagueDetail/DiscordPopup.jsx
+++ src/components/Overlays/LeagueDetail/DiscordPopup.jsx
@@ -2,36 +2,45 @@ import { useState } from "react";
 import PDFViewer from "./PDFViewer";
 import { useTranslation } from "react-i18next";
 import { IMAGES } from "../../ui/images/images";
+import { useSelector } from "react-redux";
 
 function DiscordPopup() {
   const [showModal, setShowModal] = useState(false);
 
   const handleOpen = () => setShowModal(true);
   const handleClose = () => setShowModal(false);
+  const { tournamentData } = useSelector((state) => state.tournament);
 
   const { t } = useTranslation();
 
   return (
     <>
       {/* Trigger button */}
+
       <div className="sd_rules--btn md:mb-10 md:order-1 order-2">
-        <a
-          href="https://discord.com/"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="timeline-card__header w-full md:mt-5 flex items-center gap-3 rounded-xl cursor-pointer relative sd_before sd_after px-2 py-[1rem] bg-white hover:bg-gray-100 transition-colors"
+        <button
+          onClick={(e) => {
+            e.preventDefault();
+            if (tournamentData?.discordId) {
+              window.open(
+                tournamentData.discordId,
+                "_blank",
+                "noopener,noreferrer"
+              );
+            }
+          }}
+          className="timeline-card__header w-full md:mt-5 flex items-center gap-3 rounded-xl cursor-pointer relative sd_before sd_after px-4 py-[1.4rem] bg-white hover:bg-gray-100 transition-colors"
         >
-          <span className="icon-discord flex items-center justify-center rounded-lg md:w-12 md:h-12 w-10 h-10 bg-[linear-gradient(180deg,rgba(45,46,109,1)_0%,rgba(34,35,86,1)_100%)] shadow-[inset_0_1px_4px_rgba(87,89,195,0.2)]">
-            <img src={IMAGES.discord} />
-          </span>
+          <img
+            src={IMAGES.discord}
+            alt={t("images.rules_icon")}
+            style={{ width: "1.75rem" }}
+          />
           <h3 className="timeline-card__title text-base sm:text-xl font_oswald">
             {t("rules.discord_support")}
           </h3>
-        </a>
+        </button>
       </div>
-
-      {/* Modal */}
-      {showModal && <PDFViewer onClose={handleClose} />}
     </>
   );
 }
